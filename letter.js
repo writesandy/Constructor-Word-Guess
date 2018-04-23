f81048217f2824530186562c1a216bdbe005e66c
writesandy <writesandy@gmail.com>
2018-04-22 22:09:18 -0500

    initial setup

---
+++ letter.js
@@ -0,0 +1,11 @@
+var Letter = {
+    guess = guess,
+    guessedAlready = boolean,
+    guessChecker = function() {
+        if (guessedAlready = true) {
+            // if letter has been guessed correctly then populate in string
+        } else {
+            // if letter has not been correctly guessed then populate a underscore/blank
+        }
+    }
+};
