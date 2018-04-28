65f63a50ba1f69c4cc85a102a81ca2e242fed7b3
writesandy <writesandy@gmail.com>
2018-04-27 20:14:19 -0500

    code v2

--- letter.js
+++ letter.js
@@ -1,19 +1,10 @@
 
 
 
-function Letter(char){
-
-    let chocolate = ["kit kat", "reeses cups", "chocolate cherries", "snickers"]
-
-    this.char = char;
-}
-
-
-
-function Letter = {
+function Letter () {
     this.underscore = "__";
-    this.guess = guess,
-    this.guessedAlready = false,
+    this.guess = guess;
+    this.guessedAlready = false;
     this.guessChecker = function(guessedAlready) {
         if (guessedAlready === true) {
             // if letter has been guessed correctly then populate in string
