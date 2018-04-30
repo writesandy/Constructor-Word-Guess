ce4027f317e6bb6584fe6497a3daed4fca1a6308
Sandra Peterson <writesandy@gmail.com>
2018-04-29 21:39:26 -0500

    more updates

--- Game.js
+++ Game.js
@@ -1,4 +1,17 @@
-let Word = require("./Game.js");
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// Letter.js should not require any other files.
+// Word.js should only require Letter.js
+// HINT: Write Letter.js first and test it on its own before moving on, then do the same thing with Word.js
+// HINT: If you name your letter's display function toString, JavaScript will call that function 
+// automatically whenever casting that object to a string 
+// (check out this example: https://jsbin.com/facawetume/edit?js,console)
+
+
+let Word = require("./Word.js");
 let inquirer = require("inquirer");
 
 
@@ -27,7 +40,7 @@ function Game () {
             name: 'letter',
             message: "Choose a letter",
             validate: function(val) {
-              return /[a-z1-9]/gi.test(val);
+              return /[a-z1-9]/.test(val);
             }
         }
     ]).then(function(val){
